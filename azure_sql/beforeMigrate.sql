0639108fbc98fe327577ef1411601e9ddbce9811
John McCall <jmccall@newclassrooms.org>
2016-09-07 12:47:37 -0400

    Fixed default schema (should be DBO)

--- azure_sql/beforeMigrate.sql
+++ azure_sql/beforeMigrate.sql
@@ -1,5 +1,5 @@
 /* Set schema where Flyway stores its data*/
-DECLARE @flywaySchema NVARCHAR(128) = N'administrative';
+DECLARE @flywaySchema NVARCHAR(128) = N'dbo';
 DECLARE @flywayTable NVARCHAR(128) = N'schema_changelog';
 
 /* Set new changelog table name */
